80b9160adcf7efc37cc65ba477ea6255432129f9
TuHV <tinhyeuduykhoavttl@gmail.com>
2024-01-17 13:54:17 +0700

    update code to branch abc

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,7 @@ function Home() {
     return (
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
-<<<<<<< HEAD
-            <h2>đây là branch develop</h2>
-=======
-            <h2>branch abc</h2>
->>>>>>> 2e97354 (update code to abc)
+            <h2>branch ab</h2>
         </div>
     );
 }
