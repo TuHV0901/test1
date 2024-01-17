d25af2c2053a80d7fcfd24dd8d710bc27e173d0b
TuHV <tinhyeuduykhoavttl@gmail.com>
2024-01-17 11:37:38 +0700

    update code len cho develop

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
-            <h2>Additional Heading</h2>
+            <h2>đây là branch develop</h2>
         </div>
     );
 }
