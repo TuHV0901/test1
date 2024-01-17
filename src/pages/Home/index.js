4f14b6e54387c33df0f8281e1a28fd95ad0d71b9
TuHV <104336609+TuHV0901@users.noreply.github.com>
2024-01-17 08:49:38 +0700

    Update index.js
    update code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ function Home() {
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
             <h2>Additional Heading</h2>
+            <h2>hello</h2>
         </div>
     );
 }
