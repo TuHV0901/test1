06bcecb10016c465401675215f75d71add04ce89
TuHV <tinhyeuduykhoavttl@gmail.com>
2024-01-17 12:40:22 +0700

    hi

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,11 @@ function Home() {
     return (
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
+<<<<<<< HEAD
             <h2>đây là branch develop</h2>
+=======
+            <h2>branch abc</h2>
+>>>>>>> 2e97354 (update code to abc)
         </div>
     );
 }
