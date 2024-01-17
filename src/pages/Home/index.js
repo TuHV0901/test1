2e97354c7e2ad961b3e42d10ce4ef52ae6c6981f
TuHV <tinhyeuduykhoavttl@gmail.com>
2024-01-17 12:23:14 +0700

    update code to abc

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
-            <h2>Additional Heading</h2>
+            <h2>branch abc</h2>
         </div>
     );
 }
