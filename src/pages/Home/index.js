b91278261c109e3ef4917bbcbb631d82db426016
TuHV <tinhyeuduykhoavttl@gmail.com>
2024-01-17 14:01:54 +0700

    update code lan 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
-            <h2>branch ab</h2>
+            <h2>comment 1</h2>
         </div>
     );
 }
