343e74ef0d02cf2331ff187f20a1ec22bc921e33
TuHV <tinhyeuduykhoavttl@gmail.com>
2024-01-17 11:04:16 +0700

    update code len branch main

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,6 @@ function Home() {
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
             <h2>Additional Heading</h2>
-            <h2>hello</h2>
         </div>
     );
 }
