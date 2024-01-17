81f86ebbd2494d32939ba97b449cb6ce02c80961
TuHV <tinhyeuduykhoavttl@gmail.com>
2024-01-17 08:50:53 +0700

    g

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ function Home() {
         <div>
             <h2 style={{ height: 50 }}>Home Page 1</h2>
             <h2>Additional Heading</h2>
+            <h2>Additional Heading22222222</h2>
         </div>
     );
 }
